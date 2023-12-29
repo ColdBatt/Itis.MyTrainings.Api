0bbab5a34fa75129e711e672c4eea2655ede50f9
ilyaklim23 <klimkin_ilya2004@mail.ru>
2023-12-29 19:34:46 +0300

    add UnitTestBase and InMemoryContext for tests

---
+++ Itis.MyTrainings.Api.UnitTests/Requests/User/Test.cs
@@ -0,0 +1,25 @@
+﻿using Itis.MyTrainings.Api.Core.Entities;
+using Xunit;
+
+namespace Itis.MyTrainings.Api.UnitTests.Requests.User;
+
+
+public class Test : UnitTestBase
+{
+    private UserProfile _userProfile;
+    
+    public Test()
+    {
+        _userProfile = new UserProfile()
+        {
+            Gender = "машина"
+        };
+    }
+    
+    [Fact]
+    public async Task Handle_QueryWithFilters_ShouldReturnFilteredEntitiesAsync()
+    {
+        var dbcontext = CreateInMemoryContext(
+            x => x.Add(_userProfile));
+    }
+}
